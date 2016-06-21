acc1979bcc881cdfb76a4e1c5f48db7a38303bce
KeyboardFire <andy@keyboardfire.com>
2016-06-21 12:05:24 -0400

    Finish level generation from file
    Added materials and prefabs for start, end, and regular squares.
    
    The board generator script now reads from the level file and creates the
    square according to it.

--- Assets/Src/BoardGenerator.cs
+++ Assets/Src/BoardGenerator.cs
@@ -1,15 +1,58 @@
 ﻿using UnityEngine;
-using System.Collections;
+using System.IO;
+using System.Linq;
 
 public class BoardGenerator : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
-	
-	}
-	
-	// Update is called once per frame
-	void Update () {
-	
-	}
+    enum Tile {
+        Empty, Square, Start, End, Unknown
+    }
+    public GameObject tileSquare;
+    public GameObject tileStart;
+    public GameObject tileEnd;
+
+    // called on initialization
+    void Start() {
+
+        // read the level file and turn it into Tile objects
+        var tiles =
+            from line in File.ReadAllLines("Assets/Levels/level0.txt")
+            select (
+                    from ch in line
+                    select
+                        ch == ' ' ? Tile.Empty :
+                        ch == '#' ? Tile.Square :
+                        ch == 'S' ? Tile.Start :
+                        ch == 'E' ? Tile.End :
+                        Tile.Unknown // ???
+                   );
+
+        // loop through the grid of Tiles and place them in the world
+        foreach (var x in tiles.Select((row, idx) => new { row, idx })) {
+            foreach (var y in x.row.Select((tile, idx) => new { tile, idx })) {
+
+                // the tile being placed
+                GameObject tile;
+                // the position of the tile
+                Vector3 pos = new Vector3(x.idx, 0, y.idx);
+
+                switch (y.tile) {
+                case Tile.Empty:
+                    // nop
+                    break;
+                case Tile.Square:
+                    tile = (GameObject) Instantiate(tileSquare, pos, Quaternion.identity);
+                    break;
+                case Tile.Start:
+                    tile = (GameObject) Instantiate(tileStart, pos, Quaternion.identity);
+                    break;
+                case Tile.End:
+                    tile = (GameObject) Instantiate(tileEnd, pos, Quaternion.identity);
+                    break;
+                }
+
+            }
+        }
+    }
+
 }
