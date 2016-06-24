52d6789df0fe07b056c59d55677ff2745c87ab26
Riley Inman <rileyminman@gmail.com>
2016-06-24 12:42:53 -0400

    Simplified the Controller script syntax

--- Assets/Src/Controller.cs
+++ Assets/Src/Controller.cs
@@ -3,14 +3,19 @@
 public class Controller : MonoBehaviour {
 
     public GameObject player;
+    PlayerScript playerScript;
+
+    void Awake() {
+        playerScript = player.GetComponent<PlayerScript>();
+    }
 
     void Update() {
-        if (Input.GetKeyDown(KeyCode.B)) player.GetComponent<PlayerScript>().ChangePiece(PlayerScript.PieceType.Bishop);
-        if (Input.GetKeyDown(KeyCode.K)) player.GetComponent<PlayerScript>().ChangePiece(PlayerScript.PieceType.King);
-        if (Input.GetKeyDown(KeyCode.N)) player.GetComponent<PlayerScript>().ChangePiece(PlayerScript.PieceType.Knight);
-        if (Input.GetKeyDown(KeyCode.P)) player.GetComponent<PlayerScript>().ChangePiece(PlayerScript.PieceType.Pawn);
-        if (Input.GetKeyDown(KeyCode.Q)) player.GetComponent<PlayerScript>().ChangePiece(PlayerScript.PieceType.Queen);
-        if (Input.GetKeyDown(KeyCode.R)) player.GetComponent<PlayerScript>().ChangePiece(PlayerScript.PieceType.Rook);
+        if (Input.GetKeyDown(KeyCode.B)) playerScript.ChangePiece(PlayerScript.PieceType.Bishop);
+        if (Input.GetKeyDown(KeyCode.K)) playerScript.ChangePiece(PlayerScript.PieceType.King);
+        if (Input.GetKeyDown(KeyCode.N)) playerScript.ChangePiece(PlayerScript.PieceType.Knight);
+        if (Input.GetKeyDown(KeyCode.P)) playerScript.ChangePiece(PlayerScript.PieceType.Pawn);
+        if (Input.GetKeyDown(KeyCode.Q)) playerScript.ChangePiece(PlayerScript.PieceType.Queen);
+        if (Input.GetKeyDown(KeyCode.R)) playerScript.ChangePiece(PlayerScript.PieceType.Rook);
     }
 
 }
