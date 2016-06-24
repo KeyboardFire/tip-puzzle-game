09a70d6a24a15cbb0b39bd9ecfeb523521ec3264
Riley Inman <rileyminman@gmail.com>
2016-06-24 15:33:12 -0400

    First step towards making the piece menu clickable

---
+++ Assets/Src/PieceSelection.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+
+public class PieceSelection : MonoBehaviour {
+
+    public GameObject player;
+    PlayerScript playerScript;
+
+    void Awake() {
+        playerScript = player.GetComponent<PlayerScript>();
+    }
+
+    void OnMouseDown() {
+        Debug.Log("potato");
+        switch(gameObject.name) {
+            case "king":
+                playerScript.ChangePiece(PlayerScript.PieceType.King);
+                break;
+            case "queen":
+                playerScript.ChangePiece(PlayerScript.PieceType.Queen);
+                break;
+            case "bishop":
+                playerScript.ChangePiece(PlayerScript.PieceType.Bishop);
+                break;
+            case "rook":
+                playerScript.ChangePiece(PlayerScript.PieceType.Rook);
+                break;
+            case "knight":
+                playerScript.ChangePiece(PlayerScript.PieceType.Knight);
+                break;
+            case "pawn":
+                playerScript.ChangePiece(PlayerScript.PieceType.Pawn);
+                break;
+        }
+    }
+}
