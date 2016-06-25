ed0aeacf507d9bc61ecc01480422b2c250fa5f9a
Riley Inman <rileyminman@gmail.com>
2016-06-24 22:05:46 -0400

    Added button to return to main menu

---
+++ Assets/Src/ToMenuButton.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ToMenuButton : MonoBehaviour {
+
+    public void OnClick() {
+        SceneManager.LoadScene("MenuScene");
+    }
+
+}
