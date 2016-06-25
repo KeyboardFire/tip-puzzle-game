330ad1943db549ae0a3918caebce4af3b26df0b6
KeyboardFire <andy@keyboardfire.com>
2016-06-25 10:20:28 -0400

    Refactor scene change buttons

---
+++ Assets/Src/SceneChangeButton.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneChangeButton : MonoBehaviour {
+
+    public string scene;
+
+    public void OnClick() {
+        SceneManager.LoadScene(scene);
+    }
+
+}
