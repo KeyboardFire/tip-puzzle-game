8a63d4173f538b678a3fbf3cdc942e2f08d876f5
KeyboardFire <andy@keyboardfire.com>
2016-06-29 09:10:05 -0400

    Add delay after winning

--- Assets/Src/TileEndBehavior.cs
+++ Assets/Src/TileEndBehavior.cs
@@ -1,8 +1,15 @@
-﻿using UnityEngine.SceneManagement;
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+using System.Collections;
 
 public class TileEndBehavior : TileBehavior {
 
     protected override void OnMoveSuccess() {
+        StartCoroutine(Delay(2));
+    }
+
+    IEnumerator Delay(float time) {
+        yield return new WaitForSeconds(time);
         SceneManager.LoadScene("MenuScene");
     }
 
